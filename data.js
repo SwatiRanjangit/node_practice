72a249f6ace689bcff90dcc32d9b81a06f8b1aae
Swati Ranjan <swatirp83@gmail.com>
2023-08-22 17:36:27 +0530

    first day

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: "Swati", email: "swati@gmail.com" },
+  { name: "Sam", email: "sam@gmail.com" },
+  { name: "Piyush", email: "piyush@gmail.com" },
+  { name: "Siya", email: "siya@gmail.com" },
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const fs = require("fs");
 const http = require("http");
 const colors = require("colors");
+const data = require("./data");
 
 // console.log("hello world!");
 // fs.writeFileSync("hello.txt", "hello everyone! welcome to my world!");
-http
-  .createServer((req, res) => {
-    res.write("Hello this is Swati");
-    res.end();
-    console.log("App is running...".yellow);
-  })
-  .listen(8000);
+
+// SIMPLE API......
+//DAY 1
+// http
+//   .createServer((req, res) => {
+//     // res.write("<h1>Hello this is Swati</h1>");
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.write(JSON.stringify(data));
+
+//     res.end();
+//     console.log("App is running...".yellow);
+//   })
+//   .listen(8000);
+
+// console.log("hello");
+
+// SET INPUT FROM COMMAND LINE
+
+const input = process.argv;
+// fs.writeFileSync(input[2], input[3]);
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("Invalid output".red);
+}
